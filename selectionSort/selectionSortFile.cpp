ef032a381f8b6dd39137f933e54dfb1026261ac1
Walderlan Sena <senawalderlan@gmail.com>
2017-08-31 12:05:25 -0300

    Corrigido erro na abertura de arquivo, adicionando medidas de segunça e verificação. E corrigido erro no for de impressão dos dados na tela

--- selectionSort/selectionSortFile.cpp
+++ selectionSort/selectionSortFile.cpp
@@ -7,12 +7,15 @@
  */
 #include <iostream>
 #include <cstdio>
+#include <cstdlib>
 using namespace std;
+
+#define NAMEFILE    "Lista01.txt"   //Nome do arquivo a ser lido
 
 /**
  *  Protótipos das funções
  */
-int readDataFile(void);
+void readDataFile(void);
 int coutLine(void);
 void selectionSort(int vetor[], int tam);
 
@@ -21,38 +24,47 @@ void selectionSort(int vetor[], int tam);
  */
 int main(int argc, char *argv[])
 {
+    /**
+     *  Chamando a função ler o arquivo e chama a função selectionSort para realizar a
+     *  ordenação dos dados
+     */
     readDataFile();
 
     return 0;                           //Retorna 0 para o sistema operacional
 }//end main
 
-int readDataFile()
+void readDataFile()
 {
     FILE *arquivo;          //Criando um ponteiro do tipo file, que amazenará o endereço do arquivo
     int numerosRead;        //Recebe cada linha lida em cada execução do laço
     int tam = coutLine();   //Variavel que recebe o retorno da função com a quantidade de linha do arquivo a ser ordenado
     int numTemp[tam];       //Arra que recebe as linhas lidas do arquivo
+    int i = 0;              //Variavel que realiza iterações no laço while
 
-    arquivo = fopen("Lista01.txt","r");    //Abrir o arquivo em modo de leitura (r => read)
+    arquivo = fopen(NAMEFILE,"r");    //Abrir o arquivo em modo de leitura (r => read)
 
     if (arquivo != NULL) {                 //Verifica se foi possivel abrir o arquivo
         /**
          *  Inicio de leitura do arquivo especificado
-         *  Le o arquivo e amazenar cada linha durante a iteração na Variavel numerosRead
+         *  Ler o arquivo e armazenar cada linha durante a iteração na variavel numerosRead
          *  EOF => Singnifia End Of File, ou seja, enquanto for diferente do fim do arquivo
          */
         while(fscanf(arquivo, "%d", &numerosRead) != EOF){
             numTemp[i] = numerosRead;
             i++;
         }//end while
-        selectionSort(numTemp, 12);
+        /**
+         *  Chama a função que ordena os dados
+         *  @params Array com os dados a serem ordenados
+         *  @params Tamanho de array passado
+         */
+        selectionSort(numTemp,tam);
     } else {
-        cout << "Desculpe não foi possivel abrir o arquivo !" << endl;
+        cout << "Error ! Não foi possivel abrir o arquivo !" << endl;
     }//end if
 
     fclose(arquivo);    //Fechado o arquivo
 
-    return *numTemp;
 }//end openFile
 
 /**
@@ -60,13 +72,18 @@ int readDataFile()
  */
 int coutLine()
 {
-    int tam = 0,                            //Recebe a quantidade de linhas
-        line;                               //Armazena cada linha lida
-    FILE *arq = fopen("Lista01.txt","r");   //Abri o arquivo
-    while(fscanf(arq, "%d", &line) != EOF)  //Laço que percorre as linhas do arquivo
-        tam++;                              //Incrementa a quantidade de linha obtidas
-    fclose(arq);                            //Fecha o arquivo
-    return tam;                             //Retona a quantidade de linha
+    int tam = 0,                                //Recebe a quantidade de linhas
+        line;                                   //Armazena cada linha lida
+    FILE *arq = fopen(NAMEFILE,"r");            //Abri o arquivo
+    if (arq != NULL) {
+        while(fscanf(arq, "%d", &line) != EOF)  //Laço que percorre as linhas do arquivo
+        tam++;                                  //Incrementa a quantidade de linha obtidas
+        fclose(arq);                            //Fecha o arquivo
+    } else {
+        cout << "Error ! Não foi posivel abrir o arquivo.." << endl;
+        exit(1);                                //Retorna 1 para o sistema operacional
+    }//end if
+    return tam;                                  //Retona a quantidade de linha
 }//end coutLine
 
 /**
@@ -99,7 +116,7 @@ void selectionSort(int vetor[], int tam)
         /**
          *  For que imprime o valor após cada possivel modificação
          */
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < tam; i++) {
             cout << vetor[i] << "\t";   //Cout que imprime o array com um tab
         }//end for
         cout << endl;                   //Cout quebra uma linha
